0bb785ea87234a640ad92a512a7defa0121fcb2f
Palashsoni04 <palashsoni482k@gmail.com>
2020-06-26 11:29:17 +0530

    Minor changes in UI

--- src/app/Components/publisher-news-screen/publisher-news-screen.component.ts
+++ src/app/Components/publisher-news-screen/publisher-news-screen.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/Services/news-service/news.service';
 
 @Component({
-  selector: 'app-publisher-news-screen',
-  templateUrl: './publisher-news-screen.component.html',
-  styleUrls: ['./publisher-news-screen.component.css']
+    selector: 'app-publisher-news-screen',
+    templateUrl: './publisher-news-screen.component.html',
+    styleUrls: ['./publisher-news-screen.component.css'],
 })
 export class PublisherNewsScreenComponent implements OnInit {
+    newsList: any = [];
 
-  constructor() { }
+    constructor(private newsService: NewsService) {}
 
-  ngOnInit(): void {
-  }
+    clickOnDelete(newsId) {
+        this.deleteNews(newsId);
+    }
 
+    deleteNews(newsId) {
+        this.newsService.deleteNews(newsId).subscribe(
+            (data) => {
+                this.fetchNews();
+            },
+            (error) => {
+                console.log('Error: ', error);
+            }
+        );
+    }
+
+    clickOnUpdateStatus(newsId, status) {
+        this.updateStatus(newsId, status);
+    }
+
+    updateStatus(newsId, status) {
+        this.newsService.updateStatus(newsId, status).subscribe(
+            (data) => {
+                this.fetchNews();
+            },
+            (error) => {
+                console.log('Error: ', error);
+            }
+        );
+    }
+
+    fetchNews() {
+        this.newsService.getNewsList().subscribe(
+            //this.userService.getuserlist()
+            (data) => {
+                this.newsList = data;
+                console.log(this.newsList);
+            },
+
+            (error) => {
+                console.log('Error: ', error);
+            }
+        );
+    }
+
+    ngOnInit(): void {
+        this.fetchNews();
+    }
 }
